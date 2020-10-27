dd379433cd3b497430df5ae3588cd700a5d524f9
JonathanMartinezB <Jonathanmartinezbarinas@gmail.com>
2020-10-27 17:20:58 -0500

    Agregan carpetas en views: - modules/persona - partials - public/css - public/img - publix/js

---
+++ views/partials/scripts.php
@@ -0,0 +1,20 @@
+<!-- jQuery -->
+<script src="<?= $adminlteURL ?>/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= $adminlteURL ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- bs-custom-file-input -->
+<script src="<?= $adminlteURL ?>/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= $adminlteURL ?>/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= $adminlteURL ?>/dist/js/demo.js"></script>
+<!-- Select2 -->
+<script src="<?= $adminlteURL ?>/plugins/select2/js/select2.full.min.js"></script>
+<!-- Custom Js User -->
+<script src="<?= $baseURL ?>/views/public/js/main.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        bsCustomFileInput.init();
+    });
+</script>
